101e2606366bf805622c7787aa188892fdef7ec3
Rob Cowsill <42620235+rcowsill@users.noreply.github.com>
2024-01-04 23:25:58 +0000

    Add optional summary parameter to disclosure tag

--- _plugins/show_more_tag_block.rb
+++ _plugins/show_more_tag_block.rb
@@ -1,11 +1,17 @@
 module Jekyll
   class ShowMoreTagBlock < Liquid::Block
 
+    def initialize(tag_name, markup, tokens)
+      super
+      @summary = markup.strip
+      @summary = "More..." if @summary.nil? || @summary.empty?
+    end
+
     def render(context)
       site = context.registers[:site]
       converter = site.find_converter_instance(::Jekyll::Converters::Markdown)
       text = converter.convert(super)
-      "<details><summary>More...</summary>#{text}</details>"
+      "<details><summary>#{@summary}</summary>#{text}</details>"
     end
 
   end
